eb5a863a175590476eb73de21daf85aa6adf3f06
fygo97 <fygo@hotmail.de>
2025-07-17 00:20:49 +0200

    added AdaN, corrected mistakes in logreg.py, loss.py, main.py. Pushed latest version of report

--- scripts/logreg.py
+++ scripts/logreg.py
@@ -1,4 +1,5 @@
 import copy
+import logging
 import numpy as np
 from sklearn.metrics import accuracy_score
 from sklearn.preprocessing import StandardScaler
@@ -6,6 +7,9 @@ from methods import Method, LossFunction
 from scipy.special import expit
 from tqdm import trange
 from loss import CELoss, NCCELoss
+
+logger = logging.getLogger(__name__)
+
 
 class MultivarLogReg():
 
@@ -18,14 +22,14 @@ class MultivarLogReg():
         self.num_classes = num_classes
         self.grad_norm = []
 
-
-    def fit(self, x, y, epochs, lr = 0.1, batch_size = 2048, lbd = 1e-7, alpha = 0.5, mu=0.1):
+    def fit(self, x, y, epochs, lr = 1, batch_size = 2048, lbd = 0, alpha = 1.0, mu=0.001):
         ones = np.ones(x.shape[0]).reshape((-1, 1))
         x = np.hstack([ones, x])  # Add bias column
 
         self.classes_ = np.unique(y)
         num_classes = len(self.classes_)
         self.weights = np.random.rand(num_classes, x.shape[1]) * 5
+        logger.info(f"full weights shape = {self.weights.shape}")
 
         n_samples = x.shape[0]
 
@@ -36,11 +40,12 @@ class MultivarLogReg():
 
 
         for class_idx, cls in enumerate(self.classes_):
-            if num_classes > 1:
-                binary_y = (y == cls).astype(np.float32)
-            else:
-                binary_y = y
-            weights = np.random.rand(x.shape[1]) * 0.1
+            binary_y = (y == cls).astype(np.float32)
+            if self.loss_type == LossFunction.NCCE:
+                binary_y = binary_y * 2 - 1
+            weights = np.random.randn(x.shape[1]) * 0.01
+
+            logger.info(f"single weights shape = {weights.shape}")
 
             per_class_loss = []
             per_class_grad_norm = []
@@ -100,7 +105,46 @@ class MultivarLogReg():
         H_inv = np.linalg.inv(hessian)
         weights = weights - lr * np.matmul(H_inv, error_w)
         return weights
+    
+    def AdaN(self, weights_0, H0,X,y,batch_size):
+        global AdaN_k, AdaN_Hk
+        weights_k = weights_0.copy()
+        k=0
+        d = len(weights_k)
+        while True: #"for k = 0,1,... do" <=> line 2
+            if k == 0:
+                Hk = H0
+            else:
+                Hk = Hk/4
 
+            grad_norm_k = np.linalg.norm(self.loss_function.grad(weights_k,X,y))
+            hessian_k = grad_norm_k = np.linalg.norm(self.loss_function.hessian(weights_k,X,y))
+            while True: #"repeat" <=> line 4
+                Hk = 2*Hk #line 5
+                lambda_k = np.sqrt(Hk*grad_norm_k) #line 7 (line 6 is pointless)
+
+                #define weights_plus <=> line 8--------
+                reg_hessian_k = hessian_k + lambda_k*np.eye(d)
+                p = np.linalg.solve(reg_hessian_k,grad_norm_k)
+                weights_plus = weights_k - p
+                #--------------------------------------
+
+                r_plus = np.linalg.norm(weights_plus - weights_k)# <=> line 9
+
+
+                # line 10 
+                grad_norm_plus = np.linalg.norm(self.loss_function.grad(weights_plus,X,y))
+                loss_weights_plus = self.loss_function.loss(weights_plus,X,y)
+                loss_weights_k = self.loss_function.loss(weights_k,X,y)
+                if ((grad_norm_plus <= 2* lambda_k * r_plus) and loss_weights_plus <= loss_weights_k -2/3 *lambda_k*r_plus**2 ):
+                    weights_k = weights_plus
+                    k += 1
+                    break
+        return weights_k
+
+            
+    
+    
     def perform_Mishchenko22_update_step(self, weights, x, y, lbd, H_param = 0.1 ):
         assert(self.loss_function != None)
         g = self.loss_function.grad(weights, x, y) + lbd * weights
@@ -122,7 +166,7 @@ class MultivarLogReg():
         g = self.loss_function.grad(weights, x, y)
         H = self.loss_function.hessian(weights, x, y)
         g_norm = np.sqrt(g @ np.linalg.solve(H, g))
-        alpha = (-1 + np.sqrt(1 + 2 * L_est * g_norm)) / (L_est * g_norm + 1e-12)
+        alpha = (-1 + np.sqrt(1 + 2 * L_est * g_norm)) / (L_est * g_norm)
         s = np.linalg.solve(H, g)
         weights = weights - alpha * s
         return weights
@@ -138,4 +182,3 @@ class MultivarLogReg():
         logits = np.matmul(x, self.weights.T)        # shape: (n_samples, num_classes)
         probs = expit(logits)
         return self.classes_[np.argmax(probs, axis=1)]
-
